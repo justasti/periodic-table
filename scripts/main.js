cd710005a7b2e66b7aabab30f07c71712a227710
justasti <justas.tirevicius@gmail.com>
2023-03-15 10:52:22 +0200

    feat: redesign modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,54 @@
 const elements = document.querySelectorAll('.element_wrapper')
 const backdrop = document.querySelector('.backdrop')
 const modal = document.querySelector('.modal')
+const wrapper = document.querySelector('.wrapper')
 let elementsArr = [];
 
 fetch('./scripts/elements.json')
   .then(res => res.json())
   .then(data => elementsArr = data)
 
+
 elements.forEach(el => el.addEventListener('click', () => {
   backdrop.classList.add('active')
   el.classList.add("no_hover")
-  const element = elementsArr.filter(elem => elem.symbol === el.children[1].textContent)
-  let html = `<h2>${element[0].name}</h2>`
-  Object.entries(element[0]).forEach(([key, value]) => {
-    html += `
-      <p>${key}: <strong>${value}</strong></p>
+
+  const element = elementsArr.find(elem => elem.symbol === el.children[1].textContent)
+  wrapper.classList.add(el.classList[1])
+  let html = ``
+  let oxStates;
+
+  if (typeof element.oxidationStates === 'string') {
+    oxStates = element.oxidationStates.split(',').map(state => state.trim())
+  } else if (typeof element.oxidationStates === 'number') {
+    oxStates = [element.oxidationStates]
+  }
+
+  let oxStatesHtml = ``
+  oxStates.forEach(state => {
+    if (state === 'unknown') return
+    oxStatesHtml += `
+    <p>${state}</p>
+  `})
+
+  html = `
+      <div class="top_row">
+        <p class="atomic_number">${element.atomicNumber}</p>
+        <p class='atomic_mass'>${element.atomicMass}</p>
+      </div>
+      <div class="oxidation_states">
+        ${oxStatesHtml}
+      </div>
+      <h2 class="element">${element.symbol}</h2>
+      <p class="element_name">${element.name}</p>
     `
-  })
-  modal.innerHTML = html
+
+  modal.children[0].innerHTML = html
 }))
 backdrop.addEventListener('click', () => {
   backdrop.classList.remove('active')
   elements.forEach(el => el.classList.remove("no_hover"))
+  wrapper.classList.remove('dark_blue', 'light_blue', 'red', 'purple', 'actinoids', 'lanthanoids', 'green', 'yellow', 'pink', 'grey')
 })
 modal.addEventListener('click', (e) => {
   e.stopPropagation()
